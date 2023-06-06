fd0e5a632cfb192b688cb96b168bfb52c9176881
Marquise Campbell <marquisetyrrell@gmail.com>
2023-06-06 10:05:14 -0400

    smack

--- Program.cs
+++ Program.cs
@@ -32,8 +32,15 @@ namespace ShootingDice
 
             Console.WriteLine("-------------------");
 
+            Player smack = new SmackTalkingPlayer();
+                    smack.Name = "Luke Smaktalka";
+
+            smack.Play(player3);
+
+            Console.WriteLine("-------------------");
+
             List<Player> players = new List<Player>() {
-                player1, player2, player3, large
+                player1, player2, player3, large, smack
             };
 
             PlayMany(players);
